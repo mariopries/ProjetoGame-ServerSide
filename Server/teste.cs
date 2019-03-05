45c795e216e0c58c531f843d559c7fb3a6cffe41
Douglas dos Santos <douglasdossantos22@gmail.com>
2019-03-05 00:03:30 -0300

    Ajustes

--- Server/teste.cs
+++ Server/teste.cs
@@ -1,58 +1,47 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="OperationHandlerInitial.cs" company="Exit Games GmbH">
-//   Copyright (c) Exit Games GmbH.  All rights reserved.
-// </copyright>
-// <summary>
-//   Defines the OperationHandlerInitial type.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+﻿using Photon.LoadBalancing.Common;
+using Photon.LoadBalancing.Master.OperationHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-using Photon.LoadBalancing.Common;
+#region using directives
 
-namespace Photon.LoadBalancing.Master.OperationHandler
+using ExitGames.Logging;
+
+using Photon.LoadBalancing.MasterServer;
+using Photon.LoadBalancing.Operations;
+using Photon.SocketServer;
+
+#endregion
+
+namespace Server
 {
-    #region using directives
-
-    using ExitGames.Logging;
-
-    using Photon.LoadBalancing.MasterServer;
-    using Photon.LoadBalancing.Operations;
-    using Photon.SocketServer;
-
-    #endregion
-
-    public class OperationHandlerInitial : OperationHandlerBase
+    class teste : OperationHandlerBase
     {
-        #region Constants and Fields
-
         public static readonly OperationHandlerInitial Instance = new OperationHandlerInitial();
 
         private static readonly ILogger log = LogManager.GetCurrentClassLogger();
 
-        #endregion
-
-        #region Public Methods
-
         public override OperationResponse OnOperationRequest(PeerBase peer, OperationRequest operationRequest, SendParameters sendParameters)
         {
-
-            log.DebugFormat("Douglas : OperationCode :: "+ operationRequest.OperationCode.ToString());
+            log.DebugFormat(" TESTE Douglas : OperationCode :: " + operationRequest.OperationCode.ToString());
 
             switch (operationRequest.OperationCode)
             {
                 default:
                     return HandleUnknownOperationCode(operationRequest, log);
                 case (byte)100:
-                    //log.DebugFormat("Douglas : OperationCode.Authenticate - OperationHandlerInitial.cs -- DEU CERTOOOOOOOOOOOOOOO");
+                    log.DebugFormat("Douglas : OperationCode.Authenticate - OperationHandlerInitial.cs -- DEU CERTOOOOOOOOOOOOOOO");
                     foreach (var item in operationRequest.Parameters)
                     {
                         log.DebugFormat("Douglas : " + item);
                     }
-                    
+
                     return ((MasterClientPeer)peer).HandleAuthenticate(operationRequest, sendParameters);
 
                 case (byte)OperationCode.Authenticate:
-                    //log.DebugFormat("Douglas : OperationCode.Authenticate - OperationHandlerInitial.cs");
+                    log.DebugFormat("Douglas : OperationCode.Authenticate - OperationHandlerInitial.cs");
                     return ((MasterClientPeer)peer).HandleAuthenticate(operationRequest, sendParameters);
 
                 case (byte)OperationCode.CreateGame:
@@ -63,15 +52,13 @@ namespace Photon.LoadBalancing.Master.OperationHandler
                 case (byte)OperationCode.LobbyStats:
                 case (byte)OperationCode.LeaveLobby:
                 case (byte)OperationCode.DebugGame:
-                case (byte)OperationCode.Rpc: 
+                case (byte)OperationCode.Rpc:
                     return new OperationResponse(operationRequest.OperationCode)
                     {
-                        ReturnCode = (int)Photon.Common.ErrorCode.OperationDenied, 
+                        ReturnCode = (int)Photon.Common.ErrorCode.OperationDenied,
                         DebugMessage = LBErrorMessages.NotAuthorized
                     };
             }
         }
-
-        #endregion
     }
 }
